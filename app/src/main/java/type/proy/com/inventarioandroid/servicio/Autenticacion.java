d1ae190dae9d5bbc082a4f35ef438cd9f548e6af
munozdaniel <munozda87@hotmail.com>
2014-12-20 08:04:56 -0300

    Creando clase Autenticacion.java

---
+++ app/src/main/java/type/proy/com/inventarioandroid/servicio/Autenticacion.java
@@ -0,0 +1,68 @@
+package type.proy.com.inventarioandroid.servicio;
+
+/**
+ * Created by munoz on 20/12/14.
+ *
+ */
+public class Autenticacion {
+
+    private String usuario;
+
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    private String servidor;
+
+    public String getServidor() {
+        return servidor;
+    }
+
+    public void setServidor(String servidor) {
+        this.servidor = servidor;
+    }
+    private String puerto;
+
+    public String getPuerto() {
+        return puerto;
+    }
+
+    public void setPuerto(String puerto) {
+        this.puerto = puerto;
+    }
+
+    private String directorio;
+    
+    public String getDirectorio() {
+        return directorio;
+    }
+
+    public void setDirectorio(String directorio) {
+        this.directorio = directorio;
+    }
+
+    private Boolean guardar;
+    public Boolean getGuardar() {
+        return guardar;
+    }
+
+    public void setGuardar(Boolean guardar) {
+        this.guardar = guardar;
+    }
+
+
+
+}
