db748e0cd32dcff224e6f3cfd8eb90bb440d87dc
munozdaniel <munozda87@hotmail.com>
2014-12-20 19:26:11 -0300

    Creando nuevos activities

--- app/src/main/java/type/proy/com/inventarioandroid/actividades/soporte/SoporteActivity.java
+++ app/src/main/java/type/proy/com/inventarioandroid/actividades/soporte/SoporteActivity.java
@@ -4,10 +4,22 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+
+import org.springframework.http.HttpAuthentication;
+import org.springframework.http.HttpBasicAuthentication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.concurrent.ExecutionException;
 
@@ -19,6 +31,7 @@ public class SoporteActivity extends ActionBarActivity {
     private String url = "";
     private String user ="";
     private String pass ="";
+    private Soporte soporte=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,10 +52,40 @@ public class SoporteActivity extends ActionBarActivity {
             e.printStackTrace();
         }
     }
-    private class listAllSoporteThread extends AsyncTask<Void, Void, Alumno> {
+    private class listAllSoporteThread extends AsyncTask<Void, Void, Soporte> {
         @Override
         protected Soporte doInBackground(Void... params) {
             try {
+                //Services services = null;
+                Log.v("ingresando User y Pass", user + " : " + pass);
+                // Set the username and password for creating a Basic Auth request
+                HttpAuthentication authHeader = new HttpBasicAuthentication(user, pass);
+                HttpHeaders requestHeaders = new HttpHeaders();
+                requestHeaders.setAuthorization(authHeader);
+                HttpEntity<?> requestEntity = new HttpEntity<Object>(requestHeaders);
+
+                Log.v("ingresando URL",url);
+                RestTemplate restTemplate = new RestTemplate();
+
+                MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+                converter.getObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+                restTemplate.getMessageConverters().add(converter);
+
+                // Make the HTTP GET request to the Basic Auth protected URL
+                ResponseEntity<Soporte> response = restTemplate.exchange(url, HttpMethod.GET, requestEntity, Soporte.class);
+
+                Soporte unSoporte = response.getBody();
+
+                //Log.v("leido", restLinks.getLinks().size()+"");
+                return unSoporte;
+            }
+            catch (Exception e) {
+                Log.e("main_activity", e.getMessage(), e);
+            }
+            return null;
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/type/proy/com/inventarioandroid/dom/soporte/Soporte.java
+++ app/src/main/java/type/proy/com/inventarioandroid/dom/soporte/Soporte.java
@@ -4,4 +4,114 @@ package type.proy.com.inventarioandroid.dom.soporte;
  * Created by cipoleto on 20/12/14.
  */
 public class Soporte {
+    Members members;
+
+    public Members getMembers() {
+        return members;
+    }
+
+    public void setMembers(Members members) {
+        this.members = members;
+    }
+
+    // nested class
+    public class Members{
+        private DatoString observaciones;
+        private DatoString habilitado;
+        private DatoString fecha;
+        private DatoString time_system;
+        private DatoCurso creadoPor;
+        private DatoCurso tecnico;//Tecnico
+        private DatoCurso insumos;//Insumos
+        private DatoCurso computadora;//Computadora
+        private DatoCurso estado;
+        private DatoCurso reparando;
+        private DatoCurso recepcionando;
+        private DatoCurso cancelando;
+        private DatoCurso entregando;
+        private DatoCurso esperando;
+
+        public DatoString getApellido() {
+            return apellido;
+        }
+
+        public void setApellido(DatoString apellido) {
+            this.apellido = apellido;
+        }
+
+        public DatoString getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(DatoString nombre) {
+            this.nombre = nombre;
+        }
+
+        public DatoString getDni() {
+            return dni;
+        }
+
+        public void setDni(DatoString dni) {
+            this.dni = dni;
+        }
+
+        public DatoString getFechaNacimiento() {
+            return fechaNacimiento;
+        }
+
+        public void setFechaNacimiento(DatoString fechaNacimiento) {
+            this.fechaNacimiento = fechaNacimiento;
+        }
+
+        public DatoCurso getCurso() {
+            return curso;
+        }
+
+        public void setCurso(DatoCurso curso) {
+            this.curso = curso;
+        }
+
+        // nested classes
+
+        public class DatoString {
+            String value;
+
+            public String getValue() {
+                return value;
+            }
+
+            public void setValue(String value) {
+                this.value = value;
+            }
+        }
+
+        public class DatoCurso {
+
+            DatoTitle value;
+
+            public DatoTitle getValue() {
+                return value;
+            }
+
+            public void setValue(DatoTitle value) {
+                this.value = value;
+            }
+
+            public class DatoTitle {
+                String title;
+
+                public String getTitle() {
+                    return title;
+                }
+
+                public void setTitle(String title) {
+                    this.title = title;
+                }
+            }
+        }
+
+
+
+    }
 }
+
