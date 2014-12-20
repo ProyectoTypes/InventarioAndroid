2cdcfe6acc908d2449440f116299bf6f07d9933c
munozdaniel <munozda87@hotmail.com>
2014-12-20 13:36:29 -0300

    LoginActivity: verificando datos via restful

--- app/src/main/java/type/proy/com/inventarioandroid/actividades/LoginActivity.java
+++ app/src/main/java/type/proy/com/inventarioandroid/actividades/LoginActivity.java
@@ -2,6 +2,7 @@ package type.proy.com.inventarioandroid.actividades;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,13 +14,27 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.springframework.http.HttpAuthentication;
+import org.springframework.http.HttpBasicAuthentication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.ExecutionException;
+
 import type.proy.com.inventarioandroid.R;
 import type.proy.com.inventarioandroid.servicio.Autenticacion;
 import type.proy.com.inventarioandroid.servicio.AutenticacionRepositorio;
+import type.proy.com.inventarioandroid.servicio.RestLink;
 
 
 public class LoginActivity extends ActionBarActivity {
     private Autenticacion autenticacion;
+    private String error="";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -31,10 +46,16 @@ public class LoginActivity extends ActionBarActivity {
         final EditText pass = (EditText) findViewById(R.id.txtDirectorio);
         final Button btnRegistrar = (Button) findViewById(R.id.btnRegistrar);
         final Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        final CheckBox ckbGuardarPass = (CheckBox) findViewById(R.id.ckbContrasena);
+
 
         //Obtenemos los valores del datosAutenticacion.json en una instancia de Autenticacion.
         AutenticacionRepositorio autenticacionRepositorio = new AutenticacionRepositorio();
         autenticacion = autenticacionRepositorio.datosAutenticacion(this);
+        //Llenamos los txtbox con los datos guardados en el archivo
+        usuario.setText(autenticacion.getUsuario());
+        pass.setText(autenticacion.getPassword());
+        ckbGuardarPass.setChecked(autenticacion.getGuardar());
         //Chequeamos si los valores son nulos, habilitar boton Registrarse y deshabilitar login.Viceversa
         if(!autenticacion.getUri().isEmpty())
         {
@@ -62,7 +83,7 @@ public class LoginActivity extends ActionBarActivity {
         intent.putExtra("servidor", autenticacion.getServidor());
         intent.putExtra("puerto", autenticacion.getPuerto());
         intent.putExtra("directorio", autenticacion.getDirectorio());
-        startActivityForResult( intent,1 );
+        startActivityForResult(intent, 1);
 
 
     }
@@ -73,21 +94,47 @@ public class LoginActivity extends ActionBarActivity {
      */
     public void onClickBtnLoguearse(View view)
     {
+        final EditText usuario = (EditText) findViewById(R.id.txtPuerto);
+        final EditText pass = (EditText) findViewById(R.id.txtDirectorio);
         final CheckBox ckbGuardarPass = (CheckBox) findViewById(R.id.ckbContrasena);
         Autenticacion nuevaAutenticacion = new Autenticacion();
-        nuevaAutenticacion.setUsuario(autenticacion.getUsuario());
-        nuevaAutenticacion.setDirectorio(autenticacion.getDirectorio());
-        nuevaAutenticacion.setPuerto(autenticacion.getPuerto());
-        nuevaAutenticacion.setServidor(autenticacion.getServidor());
+        if (usuario.getText().toString().isEmpty()){
+
+            generarAlerta("El campo \"usuario\" no puede estar en blanco");
+            return;
+        }
+
+        if (pass.getText().toString().isEmpty()){
+
+            generarAlerta("El campo \"contraseña\" no puede estar en blanco");
+            return;
+        }
+
+        autenticacion.setUsuario(usuario.getText().toString());
         if(!ckbGuardarPass.isChecked())
         {
-            nuevaAutenticacion.setPassword("");
-            nuevaAutenticacion.setGuardar(false);
+            autenticacion.setPassword("");
+            autenticacion.setGuardar(false);
 
         }
         else {
-            nuevaAutenticacion.setPassword(autenticacion.getPassword());
-            nuevaAutenticacion.setGuardar(true);
+            autenticacion.setPassword(pass.getText().toString());
+            autenticacion.setGuardar(true);
+        }
+        //check connection
+
+        RestLink restLinks= null;
+        try {
+            restLinks = new verificarLoginThread().execute().get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        if (restLinks == null){
+            generarAlerta(error);
+            return;
         }
         AutenticacionRepositorio autenticacionRepositorio = new AutenticacionRepositorio();
         autenticacionRepositorio.guardarDatosAutenticacion(this,nuevaAutenticacion);
@@ -102,6 +149,13 @@ public class LoginActivity extends ActionBarActivity {
 
 
     }
+
+    /**
+     * Maneja los datos que el devuelve el Activity hijo.
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         final Button btnLogin = (Button) findViewById(R.id.btnLogin);
@@ -111,23 +165,68 @@ public class LoginActivity extends ActionBarActivity {
             if(resultCode == RESULT_OK){
                btnLogin.setEnabled(true);
                 btnRegistrar.setText("Configurar URL");
-
-                Log.v("*******************", "OK devuelve true : resultCode" +resultCode);
-
             }
             if (resultCode == RESULT_CANCELED) {
                 btnLogin.setEnabled(false);
                 btnRegistrar.setText("Registrar URL");
-                Log.v("*******************", "NO devuelve false : resultCode" +resultCode);
 
             }
         }
     }
-    private void toString(CharSequence text) {
+
+    /**
+     * Muestra una pequeña ventana en el celular con un mensaje enviado por parametro.
+     * @param text
+     */
+    private void generarAlerta(CharSequence text) {
         Context context = getApplicationContext();
         int duration = Toast.LENGTH_LONG;
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
+    }
+    private class verificarLoginThread  extends AsyncTask<Void, Void, RestLink> {
+        @Override
+        protected RestLink doInBackground(Void... params) {
+            try {
+//Services services = null;
+                //Log.v("ingresando User y Pass", user + " : " + pass);
+                // Set the username and password for creating a Basic Auth request
+                HttpAuthentication authHeader = new HttpBasicAuthentication(autenticacion.getUsuario(), autenticacion.getPassword());
+                HttpHeaders requestHeaders = new HttpHeaders();
+                requestHeaders.setAuthorization(authHeader);
+                HttpEntity<?> requestEntity = new HttpEntity<Object>(requestHeaders);
+
+                Log.v("ingresando URL",autenticacion.getUri());
+                RestTemplate restTemplate = new RestTemplate();
+                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+
+                // Make the HTTP GET request to the Basic Auth protected URL
+
+                ResponseEntity<RestLink> response = null;
+
+                response = restTemplate.exchange(autenticacion.getUri(), HttpMethod.GET, requestEntity, RestLink.class);
+
+
+                //return response.getBody();
+
+                RestLink restLinks = response.getBody();
+
+                Log.v("leido", restLinks.getLinks().size()+"");
+
+
+                return restLinks;
+
+            } catch (Exception e) {
+                if (e.getMessage().contains("401")) {
+                    error = "Nombre de usuario o contraseña incorrectos.";
+                } else {
+                    error = "No se puede acceder al servidor. Verifique la dirección.";
+                }
+                Log.e("Error", "Error de conexion");
+
+            }
+            return null;
+        }
     }
 
     @Override
