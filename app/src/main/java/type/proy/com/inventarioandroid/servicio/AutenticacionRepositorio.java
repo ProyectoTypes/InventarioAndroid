d58e7f2c5c0e8f1f0aa00f0fd08730f2df75f921
munozdaniel <munozda87@hotmail.com>
2014-12-20 08:05:20 -0300

    Creando clase AutenticacionRepositorio.java

---
+++ app/src/main/java/type/proy/com/inventarioandroid/servicio/AutenticacionRepositorio.java
@@ -0,0 +1,19 @@
+package type.proy.com.inventarioandroid.servicio;
+
+import android.app.Activity;
+
+/**
+ * Created by munoz on 20/12/14.
+ */
+public class AutenticacionRepositorio {
+
+    /**
+     * Permite obtener una instancia de Autenticacion desde el archivo datos.conf guardados en el celular.
+     * En caso que no exista el archivo lo crea con valores nulos.
+     */
+    public Autenticacion autenticar(Activity activity)
+    {
+        Autenticacion autenticacion = new Autenticacion();
+
+    }
+}
