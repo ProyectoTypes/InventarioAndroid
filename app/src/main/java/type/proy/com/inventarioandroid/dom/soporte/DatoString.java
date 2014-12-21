617edd68b0719805ebfecb0ab7245272df1dd948
munozdaniel <munozda87@hotmail.com>
2014-12-21 10:47:28 -0300

    DatoString: Modificando Atributos

--- app/src/main/java/type/proy/com/inventarioandroid/dom/soporte/DatoString.java
+++ app/src/main/java/type/proy/com/inventarioandroid/dom/soporte/DatoString.java
@@ -4,4 +4,13 @@ package type.proy.com.inventarioandroid.dom.soporte;
  * Created by PROYECT TYPES on 21/12/14.
  */
 public class DatoString {
+    String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
