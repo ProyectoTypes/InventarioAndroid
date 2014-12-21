4c3d5ef59a171d575bc743409b9d52152b18712e
munozdaniel <munozda87@hotmail.com>
2014-12-21 10:47:05 -0300

    Computadora: Modificando atributos

--- app/src/main/java/type/proy/com/inventarioandroid/dom/soporte/Computadora.java
+++ app/src/main/java/type/proy/com/inventarioandroid/dom/soporte/Computadora.java
@@ -4,4 +4,17 @@ package type.proy.com.inventarioandroid.dom.soporte;
  * Created by cipoleto on 20/12/14.
  */
 public class Computadora {
+
+
+    private DatoTitle value;
+
+    public DatoTitle getValue() {
+        return value;
+    }
+
+    public void setValue(DatoTitle value) {
+        this.value = value;
+    }
+
+
 }
