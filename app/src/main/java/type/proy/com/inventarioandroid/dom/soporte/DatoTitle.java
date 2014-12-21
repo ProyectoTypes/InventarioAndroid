aec3f5832c76644e5f74e9d35212c0d48f3bf9ae
munozdaniel <munozda87@hotmail.com>
2014-12-21 10:47:51 -0300

    DatoTitle: Modificando atributos.

--- app/src/main/java/type/proy/com/inventarioandroid/dom/soporte/DatoTitle.java
+++ app/src/main/java/type/proy/com/inventarioandroid/dom/soporte/DatoTitle.java
@@ -4,4 +4,22 @@ package type.proy.com.inventarioandroid.dom.soporte;
  * Created by PROYECT TYPES on 21/12/14.
  */
 public class DatoTitle {
+        private String title;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+        private String href;
+        public String getHref() {
+            return href;
+        }
+
+        public void setHref(String href) {
+        this.href = href;
+    }
+
 }
