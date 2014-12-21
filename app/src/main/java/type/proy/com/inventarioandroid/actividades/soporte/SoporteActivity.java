0dbeee9490d166c2d98f46aef6ebdb430662ff7e
munozdaniel <munozda87@hotmail.com>
2014-12-21 09:35:50 -0300

    SoporteActivity: Obteniendo datos de un Soporte especifico.

--- app/src/main/java/type/proy/com/inventarioandroid/actividades/soporte/SoporteActivity.java
+++ app/src/main/java/type/proy/com/inventarioandroid/actividades/soporte/SoporteActivity.java
@@ -22,28 +22,110 @@
 package type.proy.com.inventarioandroid.actividades.soporte;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+
+import org.springframework.http.HttpAuthentication;
+import org.springframework.http.HttpBasicAuthentication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import type.proy.com.inventarioandroid.R;
-
+import type.proy.com.inventarioandroid.dom.soporte.Soporte;
 
 
 public class SoporteActivity extends ActionBarActivity {
+    private String url="";
+    private String user="";
+    private String pass="";
+    private Soporte soporte =null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soporte);
-    }
+        Intent intent = getIntent();
+        url =  intent.getStringExtra("url");
+        user =  intent.getStringExtra("user");
+        pass =  intent.getStringExtra("pass");
+        ListView lstComputadora = (ListView) findViewById(R.id.lstComputadora);
+        ListView lstTecnico = (ListView) findViewById(R.id.lstTecnico);
+        EditText txtObservaciones = (EditText) findViewById(R.id.mltxtObservaciones);
+        EditText dtFecha = (EditText) findViewById(R.id.dtFechaSoporte);
+        TextView lblEstado = (TextView)findViewById(R.id.lblEstado);
+        try {
+            soporte = new getSoporteThread().execute().get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        txtObservaciones.setText(soporte.getMembers().getObservaciones().getValue());
+        
 
+
+    }
+    private class getSoporteThread extends AsyncTask<Void, Void, Soporte> {
+        @Override
+        protected Soporte doInBackground(Void... params) {
+            try {
+
+
+                //Services services = null;
+                Log.v("ingresando User y Pass", user + " : " + pass);
+                // Set the username and password for creating a Basic Auth request
+                HttpAuthentication authHeader = new HttpBasicAuthentication(user, pass);
+                HttpHeaders requestHeaders = new HttpHeaders();
+                requestHeaders.setAuthorization(authHeader);
+                HttpEntity<?> requestEntity = new HttpEntity<Object>(requestHeaders);
+
+                Log.v("ingresando URL",url);
+                RestTemplate restTemplate = new RestTemplate();
+
+                MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+                converter.getObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+                restTemplate.getMessageConverters().add(converter);
+
+                // Make the HTTP GET request to the Basic Auth protected URL
+                ResponseEntity<Soporte> response = restTemplate.exchange(url, HttpMethod.GET, requestEntity, Soporte.class);
+
+                //return response.getBody();
+
+                Soporte  unSoporte = response.getBody();
+
+                //Log.v("leido", restLinks.getLinks().size()+"");
+
+
+
+                return unSoporte;
+
+            } catch (Exception e) {
+                Log.e("main_activity", e.getMessage(), e);
+            }
+
+            return null;
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
